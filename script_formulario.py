0435be80400c62d8f29cd9c9f1883aa62f34cab1
yesidmoli <nyamoli213@gmail.com>
2023-01-30 12:20:44 -0500

    Update script_formulario.py

--- script_formulario.py
+++ script_formulario.py
@@ -2,54 +2,54 @@
 import time
 
 #creation of the variable to store the information of the glasses of water
-vasos = { "yesid":0, "erick":0}
+cups = { "yesid":0, "erick":0}
 
 #menu creation
 def menu():
     print("Menu")
-    print("1) Registrar vasos de agua")
-    print("2) Ver usuarios y numero de vasos")
-    print("4) Salir")
+    print("1) Record glasses of water")
+    print("2) See users and number of glasses")
+    print("3) Go out")
 
     #selection of the option
-    opcion = int(input("Elige una opcion: "))
-    if opcion == 1:
-        registrar()
-    elif opcion == 2:
-        ver()
-    elif opcion == 3:
+    option = int(input("Choose an option: "))
+    if option == 1:
+        register()
+    elif option == 2:
+        view()
+    elif option == 3:
         exit()
     else:
-        print("Opcion no valida")
+        print("Option not valid")
 
 #function to register the glasses of water
-def registrar():
-    nombre = input("Ingresa tu nombre: ")
-    numero_de_vasos = int(input("Cuantos vasos de agua has tomado hoy: "))
+def register():
+    name = input("Enter your name: ")
+    number_cups= int(input("How many glasses of water have you had today?: "))
 
     # check if the entered user exists
-    if nombre in vasos:
-        vasos[nombre] += numero_de_vasos
+    if name in cups:
+        cups[name] += number_cups
     else:
-        print("nombre no valido, intentalo nuevamente")
+        print("Invalid name, try again")
     
 
 
     # show the dictionary
-    print("llevas registrados", vasos[nombre], "vasos de agua")
-    print("Regresando al menu...")
+    print("you have registered", cups[name], "cups of water")
+    print("Returning to the menu...")
     time.sleep(2)
     menu()
 
 
 
 #function to see the users and the number of glasses of water
-def ver():
-    for usuario, vasos_agua in vasos.items():
-        print(f"Usuario: {usuario}")
-        print(f"Vasos de agua: {vasos_agua}")
+def view():
+    for Username, cups_water in cups.items():
+        print(f"Usuario: {Username}")
+        print(f"Vasos de agua: {cups_water}")
         
-    print("Regresando al menu...")
+    print("Returning to the menu...")
     time.sleep(2)    
     menu()
 menu()
